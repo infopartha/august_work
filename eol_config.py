73d22c85462d261585159fec2c5580f971a45904
infopartha <41183333+infopartha@users.noreply.github.com>
2022-08-28 12:39:31 +0530

    Adding config file

---
+++ eol_config.py
@@ -0,0 +1,21 @@
+eol_path = '/home/prajendiran/riskfusion/sources/eol/'
+pickle_path = eol_path + 'pickles/'
+output_path = eol_path + 'predictions/'
+train_path = eol_path + 'train_data/'
+ngram_range = (3, 3)
+
+# db_name = 'rf'
+db_name = 'scap'
+
+# source_name = 'QUALYS'
+source_name = 'ALL'
+
+# read_from = 'query'
+# read_from = 'file'
+read_from = 'preprocessed'
+
+# train_file = 'eol_train_data_v6.xlsx'
+# train_file = 'eol_manipulated_data.xlsx'
+# train_file = 'eol_manipulated_data_true_only.xlsx'
+# train_file = 'eol_train_data_v9.xlsx'
+train_file = 'eol_train_data_v10.xlsx'
